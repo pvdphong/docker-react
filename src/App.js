0216067406aded885b43ef9feb09f671e6fb937a
phong <pvdphong.98@gmail.com>
2023-03-02 13:28:33 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Learn React
         </a>
+        Hello there!
       </header>
     </div>
   );
